72a5c5dcc586d4262b5277acfdf5a15d05af4331
syswraith <syswraith@proton.me>
2025-05-12 15:25:45 +0530

    permutations of the raw combinations

--- cartesian_product/main.py
+++ cartesian_product/main.py
@@ -1,9 +1,17 @@
-from itertools import product
 # importing cartesian product
+from itertools import product, permutations
 
 
-dataset = [('Lester', 'Roches', 'Nallu', 'Vincent', 'Yogibear'), ('@', '_'), ('007','23')]
 # have to typecast the numbers to strings or it doesnt work
+dataset = [('Lester', 'Roches', 'Nallu', 'Vincent', 'Yogibear'), ('@', '_'), ('007','23')]
 
-passlist = list(product(*dataset))
-for pass_tuple in passlist: print(''.join(pass_tuple))
+# first, get all cartesial products in the given order
+raw_combinations = product(*dataset)
+final_passwords = set()
+
+for combination in raw_combinations:
+    # now permutate them
+    for permutation in permutations(combination):
+        final_passwords.add(''.join(permutation))
+
+for pw in final_passwords: print(pw)
