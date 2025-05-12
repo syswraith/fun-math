3034559ca4249cf33f206a95ccbe9f9236d0dc62
syswraith <syswraith@proton.me>
2025-05-12 16:02:27 +0530

    Mon May 12 02:50:43 PM IST 2025

--- totps/main.py
+++
@@ -1,9 +0,0 @@
-import time
-from datetime import datetime
-my_epoch = datetime(1997, 8, 18)
-t_unix = time.time()
-t_interval = 30
-t_now = t_unix - my_epoch
-counter = (t_now - t_unix)/t_interval
-print(counter)
-
