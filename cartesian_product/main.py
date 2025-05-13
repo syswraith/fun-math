b37aeaf4f2ebac8f24af55354a76b826574e653e
syswraith <syswraith@proton.me>
2025-05-13 22:15:27 +0530

    Tue May 13 09:31:47 PM IST 2025

--- cartesian_product/main.py
+++ cartesian_product/main.py
@@ -1,17 +1,28 @@
-# importing cartesian product
 from itertools import product, permutations
 
+dataset = [('Lester', 'Roches', 'Nallu'), ('@', '_'), ('07', '12', '23')]
 
-# have to typecast the numbers to strings or it doesnt work
-dataset = [('Lester', 'Roches', 'Nallu', 'Vincent', 'Yogibear'), ('@', '_'), ('007','23')]
-
-# first, get all cartesial products in the given order
+# First, get all Cartesian products in the given order
 raw_combinations = product(*dataset)
 final_passwords = set()
 
+# Add the individual elements themselves to the final password set
+for tup in dataset:
+    final_passwords.update(tup)
+
+# Now permutate them and add the unique passwords
 for combination in raw_combinations:
-    # now permutate them
     for permutation in permutations(combination):
         final_passwords.add(''.join(permutation))
 
-for pw in final_passwords: print(pw)
+# Generate Cartesian products for all 2-tuple combinations
+for i in range(len(dataset)):
+    for j in range(i + 1, len(dataset)):
+        pair_combinations = product(dataset[i], dataset[j])
+        for combination in pair_combinations:
+            final_passwords.add(''.join(combination))
+
+# Print all final passwords
+for pw in final_passwords:
+    print(pw)
+
