736bfe9729bf7fd468d3ce7edbae638cb4f6b8b2
syswraith <syswraith@proton.me>
2025-05-27 12:12:36 +0530

    cartesian product

--- cartesian_product/main.py
+++ cartesian_product/main.py
@@ -1,28 +1,39 @@
-from itertools import product, permutations
+from itertools import product, permutations, combinations
 
-dataset = [('Lester', 'Roches', 'Nallu'), ('@', '_'), ('07', '12', '23')]
+dataset = [('aryan', 'pravin', 'karekar'), ('@', '_'), ('10', '04', '06')]
 
-# First, get all Cartesian products in the given order
-raw_combinations = product(*dataset)
 final_passwords = set()
 
-# Add the individual elements themselves to the final password set
+# 1. Add individual elements
 for tup in dataset:
     final_passwords.update(tup)
 
-# Now permutate them and add the unique passwords
-for combination in raw_combinations:
-    for permutation in permutations(combination):
-        final_passwords.add(''.join(permutation))
+# 2. Add intratuple combinations (e.g. aryan+pravin)
+for tup in dataset:
+    for r in range(2, len(tup) + 1):
+        for comb in combinations(tup, r):
+            for perm in permutations(comb):
+                final_passwords.add(''.join(perm))
 
-# Generate Cartesian products for all 2-tuple combinations
+# 3. Add full Cartesian product permutations
+for combination in product(*dataset):
+    for perm in permutations(combination):
+        final_passwords.add(''.join(perm))
+
+# 4. Add 2-tuple Cartesian products
 for i in range(len(dataset)):
     for j in range(i + 1, len(dataset)):
-        pair_combinations = product(dataset[i], dataset[j])
-        for combination in pair_combinations:
+        for combination in product(dataset[i], dataset[j]):
             final_passwords.add(''.join(combination))
 
-# Print all final passwords
+# 5. Add multi-tuple (r >= 2) combinations and permuted products
+for r in range(2, len(dataset) + 1):
+    for selected in combinations(dataset, r):
+        for prod in product(*selected):
+            for perm in permutations(prod):
+                final_passwords.add(''.join(perm))
+
+# Output
 for pw in final_passwords:
     print(pw)
 
